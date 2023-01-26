b77b3796d59793c00a9e20ff6b00639b25183d5a
issdilshod <dilshod@themembers.com>
2023-01-26 09:56:41 +0300

    init views

---
+++ app/Http/Controllers/Account/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    public function index()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function get()
+    {
+        //
+    }
+
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+
+}
---
+++ app/Http/Services/Account/UserService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services\Account;
+
+class UserService{
+
+    public function findAll($id = '', $name = '')
+    {
+
+    }
+
+    public function find($id = '', $name = '')
+    {
+
+    }
+
+    public function create($user)
+    {
+
+    }
+
+    public function update($user, $id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        
+    }
+
+}
