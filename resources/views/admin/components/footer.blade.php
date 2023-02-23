11140f8ae9a9676ebc4ee59ceea4b31d78b5750a
issdilshod <dilshod@themembers.com>
2023-02-23 10:46:12 +0300

    add alert to delete

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -1,3 +1,12 @@
+<script>
+    $(document).on('click', '.btn-danger', function(e){
+        if (!window.confirm("{{__('global_delete_alert')}}")){
+            e.preventDefault();
+            return false;
+        }
+    });
+</script>
+
 <!-- /.content-wrapper -->
 <footer class="main-footer">
 <strong>{{__('footer_right_reserved')}} &copy; <?=date('Y')?>.</strong>
